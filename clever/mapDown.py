a0238ebe4fc19fb277d954cf3decb9c4d6ac6ad9
matveylapin <2663161@mail.ru>
2019-07-10 14:58:58 +0300

    debug map update lib

--- clever/mapDown.py
+++ clever/mapDown.py
@@ -5,24 +5,31 @@ from time import sleep
 
 
 def map_down():
-    input_data = r.get(consts.SERVER_IP + '/static/map.txt')
-    path = consts.CLEVER_PATH + 'aruco_pose/map/map.txt'
+    while True:
+        try:
+            input_data = r.get(consts.SERVER_IP + '/static/map.txt')
+        except r.exceptions.ConnectionError:
+            sleep(5)
+            continue
+        if input_data.status_code == 200:
+            path = consts.CLEVER_PATH + 'aruco_pose/map/map.txt'
 
-    with open(path, 'r') as tempData:
-        # TODO: make dict and check
-        data = tempData.read()
-        data = data.replace('\r', '').split('\n')
+            with open(path, 'r') as tempData:
+                # TODO: make dict and check
+                data = tempData.read()
+                data = data.replace('\r', '').split('\n')
 
-        idata = input_data.text
-        idata = idata.replace('\r', '').split('\n')
+                idata = input_data.text
+                idata = idata.replace('\r', '').split('\n')
 
-        if data != idata:
-            print('Map is incorrect. Using map from server.')
-            with open(path, 'wb') as f:
-                f.write(input_data.content)
-            os.system("sudo systemctl restart clever")
-            print('Restart clever.service')
-            sleep(30)
+                if data != idata:
+                    print('Map is incorrect. Using map from server.')
+                    with open(path, 'wb') as f:
+                        f.write(input_data.content)
+                    os.system("sudo systemctl restart clever")
+                    print('Restart clever.service')
+                    sleep(30)
+                else:
+                    print('Map is correct. Starting...')
+            break
         else:
-            print('Map is corect. Starting...')
-
