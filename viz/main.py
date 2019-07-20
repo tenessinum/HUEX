5f37ea9b0396c9589cbc5af49a6240578bef6614
tinderad <44276006+tinderad@users.noreply.github.com>
2019-07-20 11:45:14 +0300

    Add new version of viz

--- viz/main.py
+++ viz/main.py
@@ -28,17 +28,74 @@ class Camera():
         self.x = pose['x']
         self.y = pose['y']
         self.z = pose['z']
-        self.p = pose['pitch'] * 56 + 130
+        self.p = pose['pitch'] * 56 + 180
         self.yw = pose['roll'] * 56 + 180
         self.r = -pose['yaw'] * 56 - 90
         print("r: " + str(self.r), self.yw, sep=" || yw: ")
         self.goto(self.x, self.y, self.z, self.p, self.r, self.yw)
+
     def update(self):
         self.goto(self.x, self.y, self.z, self.p, self.r, self.yw)
 
-def Cube(pose,size):
-    x,y,z = pose
-    glBegin(GL_POLYGON)
+
+def Grid(size):
+    z=0
+    glLineWidth(1)
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_LINES)
+    for x in range(-9,10):
+        for y in range(-5,10):
+            x*=size
+            y*=size
+            glVertex3f(x, y, z)
+            glVertex3f(x + size, y, z)
+
+            glVertex3f(x + size, y, z)
+            glVertex3f(x + size, y + size, z)
+
+            glVertex3f(x + size, y + size, z)
+            glVertex3f(x, y + size, z)
+
+            glVertex3f(x, y + size, z)
+            glVertex3f(x, y, z)
+
+    glEnd()
+
+
+def Origin():
+    glColor3f(1.0, 1.0, 1.0)
+    glBegin(GL_LINES)
+
+    glVertex3f(0, 0, 0)
+    glVertex3f(0.5, 0, 0)
+
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0.5, 0)
+
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 0.5)
+    glEnd()
+
+
+def Cube(pose, size, color):
+    nx, ny, nz = pose
+    pitch, roll, yaw = 0,0,0
+    rgb = []
+    for val in (color[1:3], color[3:5], color[5:7]):
+        val = int(val, 16)
+        rgb.append(val / 255)
+    x, y, z = (-size / 2, -size / 2, -size / 2)
+
+    glPushMatrix()
+    glTranslatef(nx, ny, nz)
+    glRotatef(pitch, 1, 0, 0)
+    glRotatef(yaw, 0, 1, 0)
+    glRotatef(roll, 0, 0, 1)
+
+    glColor3f(rgb[0], rgb[1], rgb[2])
+    glLineWidth(10)
+    glBegin(GL_LINES)
+
     glVertex3f(x, y, z)
     glVertex3f(x + size, y, z)
 
@@ -51,31 +108,32 @@ def Cube(pose,size):
     glVertex3f(x, y + size, z)
     glVertex3f(x, y, z)
     ###
-    glVertex3f(x, y, z+size)
-    glVertex3f(x + size, y, z+size)
+    glVertex3f(x, y, z + size)
+    glVertex3f(x + size, y, z + size)
 
-    glVertex3f(x + size, y, z+size)
-    glVertex3f(x + size, y + size, z+size)
+    glVertex3f(x + size, y, z + size)
+    glVertex3f(x + size, y + size, z + size)
 
-    glVertex3f(x + size, y + size, z+size)
-    glVertex3f(x, y + size, z+size)
+    glVertex3f(x + size, y + size, z + size)
+    glVertex3f(x, y + size, z + size)
 
-    glVertex3f(x, y + size, z+size)
-    glVertex3f(x, y, z+size)
+    glVertex3f(x, y + size, z + size)
+    glVertex3f(x, y, z + size)
     ###
     glVertex3f(x, y, z)
-    glVertex3f(x, y, z+size)
+    glVertex3f(x, y, z + size)
 
-    glVertex3f(x+size, y, z)
-    glVertex3f(x+size, y, z+size)
+    glVertex3f(x + size, y, z)
+    glVertex3f(x + size, y, z + size)
 
-    glVertex3f(x+size, y+size, z)
-    glVertex3f(x+size, y+size, z+size)
+    glVertex3f(x + size, y + size, z)
+    glVertex3f(x + size, y + size, z + size)
 
     glVertex3f(x, y + size, z)
     glVertex3f(x, y + size, z + size)
 
     glEnd()
+    glPopMatrix()
 
 
 def Triangle():
@@ -88,6 +146,7 @@ def Triangle():
 
 def Marker(pose, size):
     x, y, z = pose
+    glColor3f(0.4, 0.4, 0.4)
     glBegin(GL_POLYGON)
 
     glVertex3f(x, y, z)
@@ -105,11 +164,14 @@ def Marker(pose, size):
     glEnd()
 
 
-def load_markers(path):
+def load_markers():
     markers = []
-    map = urllib.request.urlopen('http://192.168.1.149:8000/static/map.txt').read()
-    for marker in list(open(path).read().split("\n")):
-        markers.append(list(marker.split(" ")))
+    map = str(urllib.request.urlopen('http://192.168.1.149:8000/static/map.txt').read())[2:].split("\\n")[:-1]
+    #map = open("map.txt").read().split("\n")
+    print(map)
+    for marker in list(map):
+        markers.append(list(marker.split(" "))[1:])
+    print(markers)
     return markers
 
 
@@ -121,17 +183,24 @@ def load_roads(path):
 
 
 def Road(x1, y1, z1, x2, y2, z2):
+    glColor3f(1.0, 1.0, 1.0)
+    glLineWidth(3)
     glBegin(GL_LINES)
     glVertex3f(x1, y1, z1)
     glVertex3f(x2, y2, z2)
     glEnd()
 
+
 def get_positions():
     copters = json.loads(urllib.request.urlopen('http://192.168.1.149:8000/get').read())["drones"]
     poses = []
     for copter in copters:
-        poses.append(copter["pose"])
+        data = [copter["pose"]]
+        data.append(copter["led"])
+        data.append(copter["status"])
+        poses.append(data)
     return poses
+
 
 def main():
     pygame.init()
@@ -139,12 +208,12 @@ def main():
     screen = pygame.display.set_mode(display, DOUBLEBUF | OPENGL | OPENGLBLIT)
     glMatrixMode(GL_PROJECTION)
     gluPerspective(60, (display[0] / display[1]), 0.1, 50.0)
-
-
+    glEnable(GL_DEPTH_TEST)
     points, roads = load_roads("roads.json")
-    markers = load_markers("map.txt")
+    print(points)
+    markers = load_markers()
     cam = Camera((0, 0, 3), (-90, 0, 0))
-    cam.goto(-1.463, -1.334, 2.665, 110.896, -149.304, 175.576)
+    cam.goto(-0.30000000000000004, -4.000000000000002, 3, -75, 0, 0)
     i = 0
     while True:
         for event in pygame.event.get():
@@ -174,31 +243,46 @@ def main():
                 elif pygame.key.get_pressed()[pygame.K_e]:
                     cam.z += 0.1
                 elif pygame.key.get_pressed()[pygame.K_h]:
-                    file = open("pose"+str(i)+".txt", "w")
-                    data=(cam.x,cam.y,cam.z,cam.p,cam.r,cam.yw)
+                    file = open("pose" + str(i) + ".txt", "w")
+                    data = (cam.x, cam.y, cam.z, cam.p, cam.r, cam.yw)
                     print("Saved!")
                     print(data)
                     for val in data:
-                        file.write(str(val)+" ")
-                        i+=1
+                        file.write(str(val) + " ")
+                        i += 1
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         cam.update()
-        #cam.move()
+        # cam.move()
+
         for marker in markers:
-            x, y, size = tuple(map(float, (marker[2], marker[3], marker[1])))
-            Marker((x, y, 0.0), size)
+            x, y, size = tuple(map(float, (marker[1], marker[2], marker[0])))
+            Marker((x, y, 0.01), size)
+
+        for point in points:
+            Cube((point['x'], point['y'],ECHELON_HEIGHT),0.05,"#ffffff")
+        for point in points:
+            Cube((point['x'], point['y'],ECHELON_HEIGHT+1),0.05,"#ffffff")
+
         for road in roads:
             x1, y1, z1 = points[road["1"]]["x"], points[road["1"]]["y"], ECHELON_HEIGHT
             x2, y2, z2 = points[road["2"]]["x"], points[road["2"]]["y"], ECHELON_HEIGHT
             Road(x1, y1, z1, x2, y2, z2)
         for road in roads:
-            x1, y1, z1 = points[road["1"]]["x"], points[road["1"]]["y"], ECHELON_HEIGHT+1
-            x2, y2, z2 = points[road["2"]]["x"], points[road["2"]]["y"], ECHELON_HEIGHT+1
+            x1, y1, z1 = points[road["1"]]["x"], points[road["1"]]["y"], ECHELON_HEIGHT + 1
+            x2, y2, z2 = points[road["2"]]["x"], points[road["2"]]["y"], ECHELON_HEIGHT + 1
             Road(x1, y1, z1, x2, y2, z2)
-        for pose in get_positions():
-            Cube((pose["x"],pose["y"],pose["z"]), 0.3)
 
+        for pose in get_positions():
+            if pose[2] == "fly":
+                Cube((pose[0]["x"],pose[0]["y"],pose[0]["z"]), 0.3,pose[1])
+            else:
+                Cube((pose[0]["x"], pose[0]["y"], 0), 0.3, pose[1])
+
+        #Cube((4, 4, 0, i, i, i), 0.3, "#ffffff")
+        Origin()
+        Grid(1)
+        i += 1
         pygame.display.flip()
         pygame.time.wait(10)
 
