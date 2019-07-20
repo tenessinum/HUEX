5a35ef4f9ec2b3c2ddefe62bb55f667ce70c2b6e
Igor Nurullaev <mighty.igor@yandex.ru>
2019-07-20 12:22:40 +0300

    Ok, this is definetly the final commit

--- server/huex/copter.py
+++ server/huex/copter.py
@@ -147,7 +147,7 @@ def check_collisions(c, copters):
                 try:
                     # print(copter.ip, 'last point and path are', copter.last_point, copter.path)
                     if copter.last_point != -1:
-                        n = int(copter.path[0][:-1])
+                        '''n = int(copter.path[0][:-1])
                         # print('My path is now', self.path)
                         nav_point = file_data['points'][n]
                         nav_point['z'] = 1.5
@@ -158,8 +158,8 @@ def check_collisions(c, copters):
                             nav_point['z'] = 2.5
                         dist = get_distance(nav_point['x'], nav_point['y'], nav_point['z'], copter.x, copter.y,
                                             copter.z)
-                        if dist < threshold:
-                            paths.append(copter.last_point)
+                        if dist < threshold:'''
+                        paths.append(copter.last_point)
                 except:
                     pass
                 try:
